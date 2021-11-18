a54a7f9beaabf68def4b63acd229963ffb3f8094
tejaswinichalla1812 <tejaswini.challa12@gmail.com>
2021-11-17 18:21:05 -0600

    Updated

--- controllers/costume.js
+++ controllers/costume.js
@@ -132,3 +132,16 @@ exports.costume_update_Page =  async function(req, res) {
         res.send(`{'error': '${err}'}`); 
     } 
 };
+// Handle a delete one view with id from query 
+exports.costume_delete_Page = async function(req, res) { 
+    console.log("Delete view for id "  + req.query.id) 
+    try{ 
+        result = await Costume.findById(req.query.id) 
+        res.render('costumedelete', { title: 'Costume Delete', toShow: 
+result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
--- routes/costume.js
+++ routes/costume.js
@@ -11,6 +11,8 @@ router.get('/detail', costume_controlers.costume_view_one_Page);
 router.get('/create', costume_controlers.costume_create_Page); 
 /* GET create update page */ 
 router.get('/update', costume_controlers.costume_update_Page);
+/* GET create costume page */ 
+router.get('/delete', costume_controlers.costume_delete_Page);
 
 
 module.exports = router; 
